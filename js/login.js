28f0095ff24bb964b7f4596ca80c12333fae0a09
ZJUZT <3120101111@zju.edu.cn>
2015-06-13 08:25:33 +0800

    add teacher register form, 真的要去吃早饭~

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@ $(function() {
         $("#teacher-register-form").delay(100).fadeIn(100);
         $("#login-form").fadeOut(100);
         $("#register-form").fadeOut(100);
-        $('login-form-link').removeClass('active');
+        $('#login-form-link').removeClass('active');
         $('#register-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
