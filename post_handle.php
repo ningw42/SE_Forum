faf9efacf67b1725d5a8954f5a318474c6e76d93
ZJUZT <3120101111@zju.edu.cn>
2015-06-14 17:52:35 +0800

    fix some bug

--- post_handle.php
+++ post_handle.php
@@ -18,19 +18,23 @@ if(isset($_POST['submit'])){ //普通帖子
 
     $sql = "select max(p_id) from posts_topic";
     $query = mysql_query($sql)
-        or die("Error1!");
+        or die("Error!");
     $row = mysql_fetch_array($query);
     $pid = $row[0]+1;
     //echo $pid;
     $sql = "insert into posts_topic (p_id,title,author_id,author,board_id,is_announcement) values ($pid,'$title',$author_id,'$author',$bid,$is_announcement)";
 
     $query = mysql_query($sql)
-        or die("Error2!");
+        or die("Error!");
 
     $sql = "insert into posts_content (p_id,content,attachment) values ($pid,'$content',$attachment)";
 
     $query = mysql_query($sql)
-        or die("Error3!");
+        or die("Error!");
+
+    $sql = "update forum_board set posts_count = posts_count + 1 where b_id = $bid";
+    $query = mysql_query($sql)
+        or die("Error!");
 
     ?>
     <script>
@@ -55,19 +59,24 @@ if(isset($_POST['submit_announcement'])){
 
     $sql = "select max(p_id) from posts_topic";
     $query = mysql_query($sql)
-    or die("Error1!");
+    or die("Error!");
     $row = mysql_fetch_array($query);
     $pid = $row[0]+1;
     //echo $pid;
     $sql = "insert into posts_topic (p_id,title,author_id,author,board_id,is_announcement) values ($pid,'$title',$author_id,'$author',$bid,$is_announcement)";
 
     $query = mysql_query($sql)
-    or die("Error2!");
+    or die("Error!");
 
     $sql = "insert into posts_content (p_id,content,attachment) values ($pid,'$content',$attachment)";
 
     $query = mysql_query($sql)
-    or die("Error3!");
+    or die("Error!");
+
+    $sql = "update forum_board set posts_count = posts_count + 1 where b_id = $bid";
+    $query = mysql_query($sql)
+    or die("Error!");
+
     ?>
     <script>
         alert("公告发布成功，跳转至公告详细页面！");
